1eba952ac8037bf6994aa8e781d34b2e2844859a
Narupan Khumpen <narupancumpen@gmail.com>
2024-11-23 19:49:27 +0700

    Fix: Dynamic port binding for deployment

--- app.py
+++ app.py
@@ -135,4 +135,9 @@ def translate_text():
         return jsonify({'translation': f'Unexpected error: {str(e)}'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the port from the environment variable or default to 5000 for local testing
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+#if __name__ == '__main__':
+#    app.run(debug=True)
